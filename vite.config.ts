c4d171cef86accde980b1b0a911419e0f96f019b
mehul <24kansalmehul@gmail.com>
2025-02-11 01:23:36 +0530

    vite.config.ts changing

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/portfolio-website/', // Ensure the base URL matches the deployment path
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
